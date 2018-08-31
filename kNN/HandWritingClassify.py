627407ebe080b4949156b051b53d6e87fb913478
lpf-gavin <lpf.gavin@foxmail.com>
2018-08-31 13:02:33 +0800

    kNN

--- kNN/HandWritingClassify.py
+++ kNN/HandWritingClassify.py
@@ -1,5 +1,6 @@
 from os import listdir
 from numpy import *
+from KNN import kNNclassify
 
 class HandWriting():
 
@@ -25,3 +26,22 @@ class HandWriting():
             for j in range(32):
                 returnVect[0,32*i+j] = int(lineStr[j])
         return returnVect
+
+def test():
+    handwriting = HandWriting()
+    testFileList = listdir('testDigits')  # iterate through the test seterrorCount = 0.0
+    errorCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]  # take off .txt
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = handwriting.img2vector('testDigits/%s' % fileNameStr)
+        classifierResult = kNNclassify(vectorUnderTest, handwriting.trainingMat, handwriting.hwLabels, 3)
+        print("the classifier came back with: %d, the real answer is: %d" % (classifierResult, classNumStr))
+        if (classifierResult != classNumStr): errorCount += 1.0
+    print("\nthe total number of errors is: %d" % errorCount)
+    print("\nthe total error rate is: %f" % (errorCount / float(mTest)))
+
+if __name__ == '__main__':
+    test()
